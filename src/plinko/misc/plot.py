9c84b6029c284dab69fe70a4fa232a23e5201c54
DeepLearning VM <google-dl-platform@googlegroups.com>
2019-11-24 09:29:06 +0000

    batch plot prediction

--- src/plinko/misc/plot.py
+++ src/plinko/misc/plot.py
@@ -4,23 +4,27 @@ import matplotlib.pyplot as plt
 from plotnine import *
 
 
-def plot_pred_target(prediction, target, sim_index):
+def plot_pred_target(prediction, target, sim_range):
     """
     :prediction: a tensor of predicted positions
     :target: a tensor of target
     :sim_index: index number of simulation to plot
     :return: print the path of target and prediction
     """
-    
-    df_pred = pd.DataFrame(prediction.data[sim_index].cpu().numpy(),columns = ['px', 'py'])
-    df_pred['source'] = 'pred'
-    df_target = pd.DataFrame(target.data[sim_index].cpu().numpy(),columns = ['px', 'py'])
-    df_target['source'] = 'target'
-    df_combined = df_pred.append(df_target)
-#         print(df_combined)
+    df = pd.DataFrame()
+    for i in sim_range:
+        df_pred = pd.DataFrame(prediction[i].data.cpu().numpy(),columns = ['px', 'py'])
+        df_pred['source'] = 'pred'
+        df_target = pd.DataFrame(target[i].data.cpu().numpy(),columns = ['px', 'py'])
+        df_target['source'] = 'target'
+        df_combined = df_pred.append(df_target)
+        df_combined['run'] = str(i)
+    #         print(df_combined)
+        df = df.append(df_combined)
+#     print(df)
 
-    p = (ggplot(df_combined, aes('px', 'py', group = 'source', color = 'source'))
-    + geom_path(alpha = .5)
-     +geom_point(alpha = .5)
-        +xlim(4, 6))
+    p = (ggplot(df, aes('px', 'py', color = 'source', grouping = 'run'))
+        + geom_path(alpha = .2)
+     +geom_point(alpha = .2)
+        +xlim(0, 10))
     print(p)
