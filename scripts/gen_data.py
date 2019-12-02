7cf969fdac9ddb09f35a0327b395250a1116ffc2
Jinxiao Zhang <jinx.zhang@stanford.edu>
2019-12-02 11:31:45 -0800

    minor

--- scripts/gen_data.py
+++ scripts/gen_data.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     # df_ball.to_feather(os.path.join('../data/training/' + 'sim_ball.feather'))
     # df_col.to_feather(os.path.join('../data/training/' + 'sim_collisions.feather'))
     # df_env.to_feather(os.path.join('../data/training/' + 'sim_environment.feather'))
-    [df_env, df_ball, df_col] = datamaker.create_sim_data(100, runs=10)
+    [df_env, df_ball, df_col] = datamaker.create_sim_data(100, runs=10, no_noise=True)
     df_ball.to_feather(os.path.join('../data/simulations/' + 'sim_ball_no_noise.feather'))
     df_col.to_feather(os.path.join('../data/simulations/' + 'sim_collisions_no_noise.feather'))
     df_env.to_feather(os.path.join('../data/simulations/' + 'sim_environment_no_noise.feather'))
